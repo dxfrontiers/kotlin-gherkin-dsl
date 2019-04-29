11f78ffde031f52a11cd69810c010c24f66b3b3b
Frank Scheffler <frank.scheffler@digitalfrontiers.de>
2019-04-29 20:31:39 +0200

    Add code for JavaLand

---
+++ README.md
@@ -0,0 +1,11 @@
+# Domain Specific Languages entwerfen mit Kotlin
+
+This project contains the source code for the talk _Sprache als Werkzeug: DSLs mit Kotlin_ held at 
+[Java Land 2020](https://programm.javaland.eu/2020/#/scheduledEvent/590715). All contents are
+© 2020 by [Digital Frontiers GmbH & Co KG](https://www.digitalfrontiers.de), unless
+stated otherwise.
+
+## Building the project
+```
+./gradlew clean build
+```
---
+++ src/main/kotlin/de/digitalfrontiers/kotlin/gherkin/GherkinDsl.kt
@@ -0,0 +1,64 @@
+package de.digitalfrontiers.kotlin.gherkin
+
+import assertk.Assert
+import assertk.assertAll
+import assertk.assertThat
+import assertk.assertions.contains
+import assertk.assertions.endsWith
+import assertk.assertions.startsWith
+
+typealias given<S> = Given<S>
+
+class Given<S>(private val setup: () -> S) {
+
+    fun <R> on(test: S.() -> R): Result<R> =
+        Result { setup().test() }
+}
+
+class Result<R>(private val result: () -> R) {
+
+    fun thenAssert(assert: Assert<R>.() -> Unit) {
+        assertThat(result()).assert()
+    }
+
+    fun then(assert: Assertions.(R) -> Unit) {
+        val assertions = Assertions()
+        assertAll {
+            assertions.assert(result())
+        }
+    }
+}
+
+class Assertions {
+    infix fun <T> T.should(asserter: Asserter<T>) {
+        asserter.assertValue(this)
+    }
+}
+
+interface Asserter<T> {
+    fun assertValue(t: T)
+}
+
+typealias startWith = StartsWithAsserter
+
+class StartsWithAsserter(private val expected: String) : Asserter<String> {
+    override fun assertValue(t: String) {
+        assertThat(t).startsWith(expected)
+    }
+}
+
+typealias contain = ContainsAsserter
+
+class ContainsAsserter(private val expected: String) : Asserter<String> {
+    override fun assertValue(t: String) {
+        assertThat(t).contains(expected)
+    }
+}
+
+typealias endWith = EndsWithAsserter
+
+class EndsWithAsserter(private val expected: String) : Asserter<String> {
+    override fun assertValue(t: String) {
+        assertThat(t).endsWith(expected)
+    }
+}
